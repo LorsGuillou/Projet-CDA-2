60cc5521c9c6b8921e0cfa1a27dd915f5651e0b1
laurent GUILLOU <laurent.guillou29200@gmail.com>
2022-10-28 10:02:31 +0200

    Premier commit

---
+++ IHM/src/main/java/com/example/projet_2/HelloController.java
@@ -0,0 +1,73 @@
+package com.example.projet_2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+public class HelloController {
+    @FXML
+    private TextField title;
+
+    @FXML
+    private ComboBox genre;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private TextField priceMin;
+
+    @FXML
+    private TextField priceMax;
+
+    @FXML
+    private CheckBox discogs;
+
+    @FXML
+    private CheckBox fnac;
+
+    @FXML
+    private CheckBox vinylcorner;
+
+    @FXML
+    private CheckBox leboncoin;
+
+    @FXML
+    private CheckBox mesvinyles;
+
+    @FXML
+    private CheckBox culturefactory;
+
+    @FXML
+    private Button search;
+
+    @FXML
+    private Button erase;
+
+    @FXML
+    private MenuItem saveOne;
+
+    @FXML
+    private MenuItem send;
+
+    @FXML
+    private MenuItem saveTwo;
+
+    @FXML
+    private MenuItem exit;
+
+    @FXML
+    protected void clean() {
+        title.setText("");
+        genre.setValue("Sélectionnez un genre");
+        date.setValue(null);
+        priceMin.setText("");
+        priceMax.setText("");
+        discogs.setSelected(false);
+        fnac.setSelected(false);
+        vinylcorner.setSelected(false);
+        leboncoin.setSelected(false);
+        mesvinyles.setSelected(false);
+        culturefactory.setSelected(false);
+    };
+}
---
+++ IHM/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.projet_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.projet_2 to javafx.fxml;
+    exports com.example.projet_2;
+}
